61e76b86c4bad914fb98b4badc7b66d24e772fa8
Kyle Selman <kyle@kyleselman.co.uk>
2014-01-08 14:30:49 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Expertse::Application.config.secret_key_base = '3de781500403a4d5471cf035c2c70201894927d36643aa58fff8f56c48ef8dbc22cfddfe233260c5c0ca29f5752ca1aec38bd2cbd5542b15eec72c9336c6f6c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Expertse::Application.config.session_store :cookie_store, key: '_expertse_session'
