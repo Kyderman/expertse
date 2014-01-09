5d64910e297e5078830b947e5688297af3b2afd0
Kyle Selman <kyle@kyleselman.co.uk>
2014-01-09 14:15:19 +0000

    set up friendhsip relations with expert

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,4 @@
 class Friendship < ActiveRecord::Base
+  belongs_to :expert
+  belongs_to :friend, :class_name => "Expert"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140109012017) do
+
+  create_table "experts", force: true do |t|
+    t.string   "firstname"
+    t.string   "surname"
+    t.string   "website"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "friendships", force: true do |t|
+    t.integer  "expert_id"
+    t.integer  "friend_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
