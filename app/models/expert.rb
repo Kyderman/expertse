deeb760727d89e2a31c7b75571a18d6a86b7557c
Kyle Selman <kyle@kyleselman.co.uk>
2014-01-13 00:16:32 +0000

    relationships now deleted along with user

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -1,9 +1,9 @@
 class Expert < ActiveRecord::Base
   
   
-  has_many :friendships
+  has_many :friendships, :dependent => :destroy
   has_many :friends, :through => :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id", :dependent => :destroy
   has_many :inverse_friends, :through => :inverse_friendships, :source => :expert
   
   has_many :tags
