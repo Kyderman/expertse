2b389784c1c0e870764adcfe0cac617c78078849
Kyle Selman <kyle@kyleselman.co.uk>
2014-02-05 16:07:38 +0000

    code clean up started

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -33,11 +33,9 @@ class TagsController < ApplicationController
 
     respond_to do |format|
       if @tag.save
-        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @tag }
+        format.html { redirect_to tags_path, notice: 'Tag was successfully created.' }
       else
         format.html { render action: 'new' }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -48,10 +46,9 @@ class TagsController < ApplicationController
   # DELETE /tags/1.json
   def destroy
     @tag.destroy
-    respond_to do |format|
-      format.html { redirect_to tags_url }
-      format.json { head :no_content }
-    end
+    redirect_to :back
+      
+    
   end
 
   private
